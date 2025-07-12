4c9e8551f169e86a359adb4af098ca83325be446
Sergey Korsensky <sergey.korsensky@intel.com>
2025-07-12 19:41:35 +0300

    Create navigation links and route patterns

--- application/routes.py
+++ application/routes.py
@@ -4,6 +4,21 @@ from flask import render_template
 
 @app.route("/")
 @app.route("/index")
+@app.route("/home")
 def index():
-    """Index route"""
-    return render_template("index.html")
+    return render_template("index.html", login=False)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html", login=False)
+
+
+@app.route("/courses")
+def courses():
+    return render_template("course.html", login=False)
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html", login=False)
