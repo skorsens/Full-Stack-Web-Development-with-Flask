3eb2f92c4ef4598e74569886f83258928692e386
Sergey Korsensky <sergey.korsensky@intel.com>
2025-07-13 12:23:19 +0300

    Add courses.html, login.html, register.html templates

--- application/routes.py
+++ application/routes.py
@@ -16,7 +16,7 @@ def login():
 
 @app.route("/courses")
 def courses():
-    return render_template("course.html", login=False)
+    return render_template("courses.html", login=False)
 
 
 @app.route("/register")
