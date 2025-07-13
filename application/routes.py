748fe6fd536ecb35993befa963b2dfaaeac577e3
Sergey Korsensky <sergey.korsensky@intel.com>
2025-07-13 12:41:52 +0300

    Pass data to courses.html

--- application/routes.py
+++ application/routes.py
@@ -6,19 +6,57 @@ from flask import render_template
 @app.route("/index")
 @app.route("/home")
 def index():
-    return render_template("index.html", login=False)
+    return render_template("index.html")
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html", login=False)
+    return render_template("login.html")
 
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html", login=False)
+    lCoursesData = [
+        {
+            "courseID": "1111",
+            "title": "PHP 101",
+            "description": "Intro to PHP",
+            "credits": 3,
+            "term": "Fall, Spring",
+        },
+        {
+            "courseID": "2222",
+            "title": "Java 1",
+            "description": "Intro to Java Programming",
+            "credits": 4,
+            "term": "Spring",
+        },
+        {
+            "courseID": "3333",
+            "title": "Adv PHP 201",
+            "description": "Advanced PHP Programming",
+            "credits": 3,
+            "term": "Fall",
+        },
+        {
+            "courseID": "4444",
+            "title": "Angular 1",
+            "description": "Intro to Angular",
+            "credits": 3,
+            "term": "Fall, Spring",
+        },
+        {
+            "courseID": "5555",
+            "title": "Java 2",
+            "description": "Advanced Java Programming",
+            "credits": 4,
+            "term": "Fall",
+        },
+    ]
+
+    return render_template("courses.html", lCoursesData=lCoursesData)
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html", login=False)
+    return render_template("register.html")
