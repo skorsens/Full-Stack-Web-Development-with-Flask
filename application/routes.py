948b1d2406ae7ac876604cfd6bf1566ba229a8a4
Sergey Korsensky <sergey.korsensky@intel.com>
2025-07-16 15:51:28 +0300

    Read the courses from the DB

--- application/routes.py
+++ application/routes.py
@@ -7,45 +7,6 @@ from application.forms import LoginForm, RegisterForm
 from application.models import Enrollment, User, Course
 
 
-lCoursesData = [
-    {
-        "courseID": "1111",
-        "title": "PHP 101",
-        "description": "Intro to PHP",
-        "credits": 3,
-        "term": "Fall, Spring",
-    },
-    {
-        "courseID": "2222",
-        "title": "Java 1",
-        "description": "Intro to Java Programming",
-        "credits": 4,
-        "term": "Spring",
-    },
-    {
-        "courseID": "3333",
-        "title": "Adv PHP 201",
-        "description": "Advanced PHP Programming",
-        "credits": 3,
-        "term": "Fall",
-    },
-    {
-        "courseID": "4444",
-        "title": "Angular 1",
-        "description": "Intro to Angular",
-        "credits": 3,
-        "term": "Fall, Spring",
-    },
-    {
-        "courseID": "5555",
-        "title": "Java 2",
-        "description": "Advanced Java Programming",
-        "credits": 4,
-        "term": "Fall",
-    },
-]
-
-
 @app.route("/")
 @app.route("/index")
 @app.route("/home")
@@ -77,6 +38,8 @@ def login():
 @app.route("/courses")
 @app.route("/courses/<term>")
 def courses(term="Spring 2019"):
+    lCoursesData = Course.objects.order_by("-courseID")
+
     return render_template(
         "courses.html", lCoursesData=lCoursesData, courses=True, term=term
     )
